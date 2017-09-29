90a6891fda429a186acc0bc68c9db643af592c7e
Uwe Kleinmann <uwe@kleinmann.org>
2017-09-29 11:34:46 +0200

    Initial commit with STARFACE client.

---
+++ Casks/starface.rb
@@ -0,0 +1,10 @@
+cask 'starface' do
+  version '1.7.0'
+  sha256 '2ba6409c0f039215b71a15d9bb0a6bf6b3fb0030a48bf8ff70ef1d8e40a25359'
+
+  url 'https://starface.de/macclient/STARFACE%20Client%201.7.0.zip'
+  name 'STARFACE Client'
+  homepage 'https://starface.de'
+
+  app 'STARFACE Client.app'
+end
