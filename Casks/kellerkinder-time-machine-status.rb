bf3f299c96a7d6db96e7def881fecfafcfdd17d2
Uwe Kleinmann <uwe@kleinmann.org>
2018-05-16 17:27:48 +0200

    [noticket] Update checksum for Kellerkinder Time Machine Status app.

--- Casks/kellerkinder-time-machine-status.rb
+++ Casks/kellerkinder-time-machine-status.rb
@@ -1,6 +1,6 @@
 cask 'kellerkinder-time-machine-status' do
-  version '1.0.0'
-  sha256 '3ffe5b3a86e43a2cb23ea1ef42b2bfe1fa3d5b1c8c42f56938752624bf13cc68'
+  version '1.0.1'
+  sha256 '3c5aa57e3ccfe777778336633416c15a094cc97816e90788b8c5d15d3cd65e05'
 
   url 'https://s3-eu-west-1.amazonaws.com/kellerkinder-staff/Kellerkinder+Time+Machine+Status.app.zip'
   name 'Kellerkinder Time Machine Status'
